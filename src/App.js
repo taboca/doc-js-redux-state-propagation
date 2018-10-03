8ed1060213df84ba44b2191ac60c7d92c21d5589
Marcio Galli <taboca@gmail.com>
2018-10-03 16:46:28 -0300

    Demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TestActivity from './containers/testActivity';
+import TestActivityA from './containers/testActivityA';
+import TestActivityB from './containers/testActivityB';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <div>
+        <TestActivity />
+        <TestActivityA />
+        <TestActivityB />
+        </div>
       </div>
     );
   }
